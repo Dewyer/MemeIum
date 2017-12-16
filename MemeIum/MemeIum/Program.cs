dac65bf66f1b239905a818ac41409f124422038b
Rátki Barnabás <barna.ratki@gmail.com>
2017-12-16 22:10:13 +0100

    Init

---
+++ MemeIum/MemeIum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemeIum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
