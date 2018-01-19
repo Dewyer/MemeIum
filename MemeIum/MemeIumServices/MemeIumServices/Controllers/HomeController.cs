9f958e952f8ae1198d0c94bb1b3364e74283708e
Rátki Barnabás <barna.ratki@gmail.com>
2018-01-19 12:58:09 +0100

    All fun and games

---
+++ MemeIum/MemeIumServices/MemeIumServices/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MemeIumServices.Models;
+
+namespace MemeIumServices.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Wallet()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MemeIum/MemeIumServices/MemeIumServices/wwwroot/js/transmaker.js
@@ -0,0 +1,20 @@
+﻿
+$("#privkey").change(privChanged);
+
+function hexToBase64(hexstring) {
+    return btoa(hexstring.match(/\w{2}/g).map(function (a) {
+        return String.fromCharCode(parseInt(a, 16));
+    }).join(""));
+}
+
+function privChanged() {
+    var outp = $.parseXML($("#privkey").val());
+    var ses = $(outp);
+    var mod = ses.find("Modulus").text();
+    var exp = ses.find("Exponent").text();
+    var md = forge.md.sha256.create();
+    md.update(exp + " " + mod);
+    var addr = hexToBase64(md.digest().toHex());
+    console.log(addr);
+    $("#addr").val(addr);
+}
